603cae6bdc5cb2151892e63e6fc11a65df463fb9
Cameron Paul <cpaul37@gmail.com>
2017-04-27 23:41:05 -0500

    Wrap code in `main` function

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
-var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+function main() {
+    var app = express();
 
-app.listen(5000, function() {
-    console.log('Running on port 5000...');
-});
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+
+    app.listen(5000, function() {
+        console.log('Running on port 5000...');
+    });
+}
+
+if (require.main === module) {
+    main();
+}
